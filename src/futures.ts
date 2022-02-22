8ab45ed458739c76759ac45b9967d3e33c88e70c
Troy <troy.burmeister@gmail.com>
2022-02-22 16:27:49 -0700

    add volume

--- src/futures.ts
+++ src/futures.ts
@@ -65,6 +65,7 @@ export function handlePositionModified(event: PositionModifiedEvent): void {
     statEntity.pnlWithFeesPaid = ZERO;
     statEntity.liquidations = ZERO;
     statEntity.totalTrades = ZERO;
+    statEntity.totalVolume = ZERO;
   }
   if (event.params.tradeSize.isZero() == false) {
     let tradeEntity = new FuturesTrade(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
@@ -123,6 +124,9 @@ export function handlePositionModified(event: PositionModifiedEvent): void {
     if (exitPrice) {
       statEntity.pnl = statEntity.pnl.plus(
         positionEntity.size.times(exitPrice.minus(positionEntity.entryPrice)).div(ETHER),
+      );
+      statEntity.totalVolume = statEntity.totalVolume.plus(
+        positionEntity.size.times(positionEntity.entryPrice).div(ETHER),
       );
     }
   } else {
